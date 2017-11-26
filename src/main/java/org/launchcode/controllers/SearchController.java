7e4c2843973976088958dcbc7e1cb60b2b01c22b
Mike Champion <championmike42@gmail.com>
2017-11-26 15:10:08 -0600

    Finished project commit, might add more bonus missions

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,10 +4,14 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+
+import static org.launchcode.controllers.ListController.columnChoices;
 
 /**
  * Created by LaunchCode
@@ -18,10 +22,39 @@ public class SearchController {
 
     @RequestMapping(value = "")
     public String search(Model model) {
-        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("columns", columnChoices);
         return "search";
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results", method=RequestMethod.GET)
+    public String searchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        int number = 0;
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("header", "All Jobs");
+            model.addAttribute("jobs", jobs);
+            model.addAttribute("columns", columnChoices);
 
+            for (HashMap<String, String> job : jobs) {
+                number++;
+                String results = Integer.toString(number) + " Result(s)";
+                model.addAttribute("results", results);
+            }
+        } else {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("header", "Jobs with " + columnChoices.get(searchType) + ":" + columnChoices.get(searchTerm));
+            model.addAttribute("jobs", jobs);
+            model.addAttribute("columns", columnChoices);
+
+            for (HashMap<String, String> job: jobs) {
+                number++;
+            String results = Integer.toString(number) + " Result(s)";
+            model.addAttribute("results", results);
+        }
+
+        }
+        return "search";
+
+    }
 }
